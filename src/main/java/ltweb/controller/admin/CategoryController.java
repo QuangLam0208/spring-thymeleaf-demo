ad87874090407744d8e59af95df946f99a3c807f
Quang Lam <laulua1223@outlook.com>
2025-12-05 10:44:59 +0700

    add ajax cate

--- src/main/java/ltweb/controller/admin/CategoryController.java
+++ src/main/java/ltweb/controller/admin/CategoryController.java
@@ -43,6 +43,11 @@ public class CategoryController {
 
     @Autowired
     private UserService userService;
+    
+    @GetMapping("/ajax")
+    public String listAjax() {
+        return "admin/categories/list_ajax";
+    }
 
     @ModelAttribute("users")
     public List<User> getUsers() {
